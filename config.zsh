6455dfd8c1a2a8ed28f7cb13a12a310180ed3e96
Carl 'Cid' Romero <carlcidromero@gmail.com>
2022-08-11 04:41:00 -0700

    Organize zsh config

--- config.zsh
+++ config.zsh
@@ -8,11 +8,18 @@
 #   source $CONFIGZSH
 # fi
 
+# Set nvim as default editor
 export EDITOR=/usr/bin/nvim
-alias vim='nvim'
-alias mv='mv -i'
+
+# Aliases
 alias cp='cp -i'
+alias mv='mv -i'
+alias vim='nvim'
+
+# Use vim on command line
 bindkey -v
+
+# Change cursor per vim mode on command line
 bindkey -M viins 'jk' vi-cmd-mode
 bindkey -M viins 'kj' vi-cmd-mode
 VI_MODE_SET_CURSOR=true
