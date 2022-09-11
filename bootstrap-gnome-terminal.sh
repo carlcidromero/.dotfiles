8d39d98fb57b655aa55b6eda11b839dc91d43f00
Carl (Cid) Romero <carlcidromero@gmail.com>
2022-09-11 15:57:23 -0700

    Feat: Create catppuccin gnome terminal profile script

---
+++ bootstrap-gnome-terminal.sh
@@ -0,0 +1,5 @@
+# create catppuccin gnome terminal profile
+git clone https://github.com/catppuccin/gnome-terminal ~/catppuccin-gnome-terminal-profile
+cd ~/catppuccin-gnome-terminal-profile
+bash ./install.sh "Catppuccin"
+
