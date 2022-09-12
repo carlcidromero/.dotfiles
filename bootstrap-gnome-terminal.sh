eec23be70dfcc6dca5cbc64f6e5ddfe6be5b0ab8
Carl (Cid) Romero <carlcidromero@gmail.com>
2022-09-11 23:08:49 -0700

    Refactor: Clean up gnome terminal bootstrapper

--- bootstrap-gnome-terminal.sh
+++ bootstrap-gnome-terminal.sh
@@ -1,5 +1,8 @@
 # create catppuccin gnome terminal profile
-git clone https://github.com/catppuccin/gnome-terminal ~/catppuccin-gnome-terminal-profile
-cd ~/catppuccin-gnome-terminal-profile
-bash ./install.sh "Catppuccin"
+TARGET_DIRECTORY=~/.catpuccin-gnome-terminal-profile
+NEW_PROFILE_NAME="Catppuccin"
+
+git clone https://github.com/catppuccin/gnome-terminal $TARGET_DIRECTORY
+cd ~$TARGET_DIRECTORY
+./install.sh $NEW_PROFILE_NAME
 
