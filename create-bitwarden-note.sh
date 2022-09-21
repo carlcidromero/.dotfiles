27f69385a85a2d558bc3708f002ce7a4242b4062
cid <carlcidromero@gmail.com>
2022-09-21 07:39:03 -0700

    Fix: Expand vars in bash script

--- create-bitwarden-note.sh
+++ create-bitwarden-note.sh
@@ -5,5 +5,5 @@ echo "Let's add a secure note to your Bitwarden vault..."
 read -p "What would you like to write in the note? " CONTENT
 read -p "What would you like to call the note? " NAME
 
-bw get template item | jq '.type = 2 | .secureNote.type = 0 | .notes = "\"$CONTENT"\" | .name = "\"$NAME"\"' | bw encode | bw create item
+bw get template item | jq ".type = 2 | .secureNote.type = 0 | .notes = \"$CONTENT\" | .name = \"$NAME\"" | bw encode | bw create item
 
