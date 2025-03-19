75f6e7bd0352dbe5db5fe8b071266a033ab22391
ciiid <carlcidromero@gmail.com>
2025-03-18 19:05:26 -0700

    Unbind jk/kj

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,4 @@
 require("config.options")
-require("config.keymaps")
 require("config.lazy")
 require("config.commands")
 require("config.vim-prettier")
--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
-vim.keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true })
